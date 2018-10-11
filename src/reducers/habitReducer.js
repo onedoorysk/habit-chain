06f8e7df95f157ca926574017b127611a71ec70f
onedoorysk <rysk1008@gmail.com>
2018-10-11 23:34:53 +0900

    refector habitReducer

--- src/reducers/habitReducer.js
+++ src/reducers/habitReducer.js
@@ -1,34 +1,10 @@
 import TYPE from '../actions/_actionType'
 import v4 from 'uuid/v4'
-import axios from 'axios'
-
-async function getInitialHabit() {
-  return await axios.get('/habits')
-}
 
 export default (state = [], {type, payload}) => {
-  state = getInitialHabit()
-    .then(resolve => {
-      console.log(resolve.data)
-      return resolve.data
-    })
-  console.log(state)
   switch(type) {
     case TYPE.ADD_HABIT:
       const {habitName, description} = payload
-      axios.post('/habits', {
-        id: v4(),
-        habitName,
-        description,
-        completed: false,
-        chainCount: 0
-      }).then(response => {
-        console.log(response)
-      })
-      .catch(err => {
-        console.log(new Error(err))
-      })
-      // const newState = axios.get('/api/habit')
       return [...state, {id: v4(), habitName, description, completed: false, chainCount: 0}]
     case TYPE.DELETE_HABIT:
       console.log(state, payload.id)
